39f787bd3481ca55f43d000cf105c5f8b6936343
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-01-28 16:29:35 +0200

    Added components for the home,landingPage,logIn,profile and signUp pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LandingPageComponent,
+    LogInComponent,
+    ProfileComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
