e021e12578c642233435e04fdc2eab818acb4bc2
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-01-28 18:03:17 +0200

    adding angular materials to give a bit of style to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +7,9 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,16 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     LandingPageComponent,
     LogInComponent,
     ProfileComponent,
-    SignUpComponent
+    SignUpComponent,
+    MatToolbarModule,
+    MatSliderModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
