242a540727c770f7b72939d3f344cd0594aa458c
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-02-08 18:28:20 +0200

    connecting to FirebaseDB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
+import { environment } from '../environments/environment'
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { Database, getDatabase, ref, set, onValue } from "firebase/database";
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { v4 as uuidv4 } from 'uuid';
+import { Chat } from '../chat/chat'
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//specifiying properties of the chat message
 export class AppComponent {
   title = 'ChatApp';
+  app: FirebaseApp;
+  db: Database;
+  form: FormGroup;
+  username = '';
+  message = '';
+  chats: Chat[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.app = initializeApp(environment.firebase);
+    this.db = getDatabase(this.app);
+    this.form = this.formBuilder.group({
+      'message': [],
+      'username': []
+    });
+    
+  }
+//pushing messages to firebase
+  onChatSubmit(form: any) {
+    const chat = form;
+    chat.timestamp = new Date().toString();
+    chat.id = uuidv4();
+    set(ref(this.db, `chats/${chat.id}`), chat);
+    this.form = this.formBuilder.group({
+      'message': [],
+      'username': [chat.username],
+    });
+  }
+  ngOnInit(): void {
+    const chatsRef = ref(this.db, 'chats');
+    onValue(chatsRef, (snapshot: any) => {
+      const data = snapshot.val();
+      for (let id in data) {
+        if (!this.chats.map(chat => chat.id).includes(id)) {
+          this.chats.push(data[id])
+        }
+      }
+    });
+  }
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,25 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// My web app's Firebase configuration
+
+//If you run the project like this you will be using my Firebase DB
+
+// If anyone wants to use their own firebase DB they need to  change the code below with your own firebase DB
+// located in the App Project information in FireBase
 
 export const environment = {
+  firebase:{
+  apiKey: "AIzaSyBq5Ycxuj6ZD5peACUja5Jq94ph_nmW1xo",
+  authDomain: "firechatapp-804f2.firebaseapp.com",
+  databaseURL: "https://firechatapp-804f2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "firechatapp-804f2",
+  storageBucket: "firechatapp-804f2.appspot.com",
+  messagingSenderId: "867968632264",
+  appId: "1:867968632264:web:0f44550813676382822cfd"
+},
   production: false
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
